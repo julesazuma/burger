bbc14a7422f0b5b4136d1bb35349dd6eb56c998d
PANDAmonium <jazuma86@yahoo.com>
2018-12-18 18:08:13 -0800

    modified server.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', FALSE);
+ 
--- server.js
+++ server.js
@@ -15,10 +15,10 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgersController.js");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
-app.listen(PORT, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port:%s", PORT);
 });
